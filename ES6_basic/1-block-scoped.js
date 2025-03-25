9aae554be114eb424ba8ed7cf563c6e58899a76b
koryos77 <k.rouget21@gmail.com>
2025-03-25 13:01:25 +0100

    change indentation

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
-  
+
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const innerTask = true;
+    const innerTask2 = false;
+    console.log(innerTask, innerTask2);
   }
-  
+
   return [task, task2];
 }
--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (let value of array) {
-        let idx = array.indexOf(value);
-        array[idx] = appendString + value;
-    }
-  
-    return array;
+  for (let value of array) {
+    let idx = array.indexOf(value);
+    array[idx] = appendString + value;
   }
+  
+  return array;
+}
