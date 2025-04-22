300ba5724a93e7570caa57a993153304e1f8b4f2
koryos77 <k.rouget21@gmail.com>
2025-04-22 13:50:49 +0200

    docstring

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+
 def add(a: float, b: float) -> float:
-    """Returns the sum of two floats"""
+    """Returns the sum of two floating-point numbers.
+    
+    Args:
+        a (float): First number to add
+        b (float): Second number to add
+    
+    Returns:
+        float: The sum of a and b
+    """
     return a + b
