11394c72ae07f8d260ead460285e96172a05b5f3
koryos77 <k.rouget21@gmail.com>
2025-04-22 14:01:03 +0200

    docstring

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 import math
+"""Return the floor of the float as an integer"""
 
-def floor(n: float) -> float:
-    """Return the floor of the float"""
+
+def floor(n: float) -> int:
+    """Return the floor of the float as an integer
+
+    Args:
+        n (float): The number
+
+    Returns:
+        int: The biggest integer less than or equal to the number"""
     return math.floor(n)
--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
+"""Returns the string representation of the float"""
+
+
 def to_str(n: float) -> str:
-    """Returns the string representation of the float"""
+    """Returns the string representation of the float
+
+    Args:
+        n (float): floor
+
+    Returns:
+        str: representation of the float
+    """
     return str(n)
