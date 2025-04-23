ecbaacd778edf636e50f3e6c8c73bd45b1a9ee51
koryos77 <k.rouget21@gmail.com>
2025-04-23 16:48:46 +0200

    fixed the method of task 2

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -4,7 +4,7 @@ that measures the total execution time for wait_n(n, max_delay),
 and returns total_time / n."""
 import asyncio
 import time
-wait_n = __import__('1-concurrent_coroutines')
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
@@ -15,8 +15,9 @@ def measure_time(n: int, max_delay: int) -> float:
         n (int): number of times to spawn wait_random in wait_n
         max_delay (int): Max delay for wait_n
     """
-    start_time = time.time()
+    start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    total_time = time.time() - start_time
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
 
-    return total_time
+    return total_time / n
